a66e143382cb613702fa2bda2bbec78c518abdbd
ronaldcc25 <racalderon13@utpl.edu.ec>
2026-06-11 12:12:47 -0500

    cambio2

--- ejemplo07/ejemplo01/academia/models.py
+++ ejemplo07/ejemplo01/academia/models.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from django.db import models
 
 # Create your models here.
@@ -14,4 +16,17 @@ class Estudiante(models.Model):
     edad = models.IntegerField()
 
     def __str__(self):
-        return f"Nombre: {self.nombre} - Apellido: {self.apellido} - Cedula: {self.cedula} - edad: {self.edad}"
+        return f"Nombre: {self.nombre} - Apellido: {self.apellido} - Cedula: {self.obtener_ciudad()} - edad: {self.edad} - año nacimiento: {self.obtener_anio()}"
+
+    def obtener_anio(self):
+        anio_actual = datetime.now().year
+        return anio_actual - self.edad
+
+    """
+    hacer que se oculte la cedula si empieza la cedula por 11 diga Loja en lugar de cedula y si es cualquier otra cedula que diga Otra ciudad"""
+
+    def obtener_ciudad(self):
+        if self.cedula.startswith("11"):
+            return "Loja"
+        else:
+            return "Otra ciudad"
